4049a556e1104ac310e0faaec4b9a7dd477a5fbe
Emelyn Ochoa <emelynochoa@Emelyns-MacBook-Air.local>
2025-02-07 15:33:09 -0800

    Deleted unnecessary text

--- assignment.py
+++ assignment.py
@@ -3,14 +3,7 @@ import scipy.stats as stats
 
 class SignalDetection:
     def __init__(self, hits, misses, false_alarms, correct_rejections):
-        """
-        Initialize the Signal Detection class with required counts.
-        
-        :hits: Number of correctly detected signals
-        :misses: Number of signals missed
-        :false_alarms: Number of noise trials incorrectly detected as signals
-        :correct_rejections: Number of correctly identified noise trials
-        """
+       
         self.hits = hits
         self.misses = misses
         self.false_alarms = false_alarms
@@ -24,4 +17,3 @@ class SignalDetection:
    
     def false_alarm_rate(self):
         """Calculate flase alarm rate (FA)"""
-        
