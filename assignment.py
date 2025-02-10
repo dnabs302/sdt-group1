6dc84c3958d0ff0d442b60d1ebadf6259daf4403
dnabar <dnabs302>
2025-02-10 05:45:17 +0000

    added functions - d_prime and criterion

--- assignment.py
+++ assignment.py
@@ -1,5 +1,9 @@
-
 import scipy.stats as stats
+from scipy.stats import norm
+import statistics as st
+import unittest
+import numpy as np
+import matplotlib.pyplot as plt
 
 class SignalDetection:
     def __init__(self, hits, misses, false_alarms, correct_rejections):
@@ -20,4 +24,13 @@ class SignalDetection:
         total_noises = self.false_alarms + self.correct_rejections
         FA = self.false_alarms / total_noises if total_noises > 0 else 0.0001
         return min(max(FA, 0.0001), 0.9999) 
+
+    def d_prime(self):
+        Hr = self.hit_rate()
+        FA = self.false_alarm_rate()
+        return norm.ppf(Hr) - norm.ppf(FA)
     
+    def criterion(self):
+        Hr = self.hit_rate()
+        FA = self.false_alarm_rate()
+        return -0.5 * (norm.ppf(Hr) + norm.ppf(FA))
